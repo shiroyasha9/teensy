8a6fe4e2190af97c5293dd31c64f8dea7181f745
Mubin Ansari <mubinansari910@gmail.com>
2022-12-27 15:08:18 +0530

    use rubik font from next/font instead of manual font face

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,25 @@
-import "../styles/globals.css";
+import { Rubik } from "@next/font/google";
 import type { AppType } from "next/app";
+
+import "../styles/globals.css";
+
 import { trpc } from "../utils/trpc";
 
+export const rubik = Rubik({
+  subsets: ["latin"],
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${rubik.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
