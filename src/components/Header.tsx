12372e10d1d27c653569104ba427e86f959ebc8c
Mubin Ansari <mubinansari910@gmail.com>
2023-01-22 18:44:28 +0530

    modify header component and remove redundant related files

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,73 +1,128 @@
+import { Popover, Transition } from "@headlessui/react";
 import { useSetAtom } from "jotai";
 import { signOut, useSession } from "next-auth/react";
-import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import useMediaQuery from "../hooks/useMediaQuery";
+import { Fragment } from "react";
+import { HiBars3, HiXMark } from "react-icons/hi2";
 import { showAuthModalAtom } from "../stores";
 import Button from "./Button";
-import MobileHeader from "./MobileHeader";
+import GitHubIcon from "./GitHubIcon";
 
-const Header = () => {
-  const router = useRouter();
-  const at = router.asPath;
-  const isBreakpointReached = useMediaQuery(768);
+const navItems = [
+  {
+    name: "🤏 Teensy a link",
+    href: "/",
+  },
+  {
+    name: "💬 Contacless WhatsApp",
+    href: "/wa",
+  },
+  {
+    name: "🔗 My Teensies",
+    href: "/teensies",
+  },
+];
+
+export default function Header() {
   const { data: session } = useSession();
   const setShowAuthModal = useSetAtom(showAuthModalAtom);
 
   const user = session?.user;
 
-  if (isBreakpointReached) return <MobileHeader />;
-
   return (
-    <>
-      <header className="body-font hidden md:block">
-        <div className="container mx-auto flex flex-col flex-wrap items-center p-5 md:flex-row">
-          <Link href="/">
-            <span className="text-4xl text-lemon-400">teensy</span>
-          </Link>
-          <nav className="mt-3 flex flex-row flex-wrap items-center justify-center text-base md:ml-auto md:mt-0 md:flex-row">
-            {at === "/" ? (
-              <Link href={"/wa"} className="mr-0 hover:text-lemon-400 md:mr-5">
-                💬 Contactless WhatsApp
-              </Link>
-            ) : (
-              <Link href={"/"} className="mr-0 hover:text-lemon-400 md:mr-5">
-                🤏 Teensy a link
-              </Link>
-            )}
-            {user && (
+    <Popover className="relative">
+      <div className="mx-auto max-w-7xl px-6">
+        <div className="flex items-center justify-between  py-6 md:justify-start md:space-x-10">
+          <div className="flex justify-start lg:w-0 lg:flex-1">
+            <Link href="/">
+              <span className="text-4xl text-lemon-400">teensy</span>
+            </Link>
+          </div>
+          <div className="-my-2 -mr-2 flex items-center md:hidden">
+            <GitHubIcon />
+            <Popover.Button className="inline-flex items-center justify-center rounded-md p-2 text-lemon-400 hover:text-lemon-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-lemon-400">
+              <span className="sr-only">Open menu</span>
+              <HiBars3 className="h-6 w-6" aria-hidden="true" />
+            </Popover.Button>
+          </div>
+          <Popover.Group as="nav" className="hidden space-x-10 md:flex">
+            {navItems.map((item) => (
               <Link
-                className="mr-0 hover:text-lemon-400 md:mr-5"
-                href="/teensies"
+                key={item.name}
+                href={item.href}
+                className=" hover:text-lemon-400"
               >
-                🔗 My Teensies
+                {item.name}
               </Link>
-            )}
-            <a
-              className="mr-0 hover:text-lemon-400 md:mr-5"
-              href="https://github.com/shiroyasha9/teensy/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <Image
-                src="/github.svg"
-                alt="github icon"
-                width="30"
-                height="30"
-              />
-            </a>
-          </nav>
-          <Button
-            variant="primary"
-            title={user ? "Logout" : "Login"}
-            className=" m-0 border-0 py-1 px-3 text-base focus:outline-none md:mt-0"
-            onClick={() => (user ? void signOut() : setShowAuthModal(true))}
-          />
+            ))}
+          </Popover.Group>
+          <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+            <GitHubIcon />
+            <Button
+              title={user ? "Logout" : "Login"}
+              className="m-0 ml-8 py-1 px-3 text-base"
+              onClick={() => (user ? void signOut() : setShowAuthModal(true))}
+            />
+          </div>
         </div>
-      </header>
-    </>
-  );
-};
+      </div>
 
-export default Header;
+      <Transition
+        as={Fragment}
+        enter="duration-200 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <Popover.Panel
+          focus
+          className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden"
+        >
+          <div className="divide-y-2 divide-gray-400 rounded-lg bg-gray-800 shadow-lg ring-1 ring-black ring-opacity-5">
+            <div className="px-5 pt-5 pb-6">
+              <div className="flex items-center justify-between">
+                <div>
+                  <span className="text-2xl text-lemon-400">teensy</span>
+                </div>
+                <div className="-mr-2">
+                  <Popover.Button className="inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-lemon-400">
+                    <span className="sr-only">Close menu</span>
+                    <HiXMark className="h-6 w-6" aria-hidden="true" />
+                  </Popover.Button>
+                </div>
+              </div>
+              <div className="mt-6">
+                <nav className="grid gap-y-8">
+                  {navItems.map((item) => (
+                    <Link
+                      key={item.name}
+                      href={item.href}
+                      className="-m-3 flex items-center rounded-md p-3 hover:text-lemon-400"
+                    >
+                      <span className="ml-3 text-base text-gray-50">
+                        {item.name}
+                      </span>
+                    </Link>
+                  ))}
+                </nav>
+              </div>
+            </div>
+            <div className="space-y-6 py-6 px-5">
+              <div>
+                <Button
+                  title={user ? "Logout" : "Login"}
+                  className="m-0 w-full border border-transparent px-4 py-2 text-base"
+                  onClick={() =>
+                    user ? void signOut() : setShowAuthModal(true)
+                  }
+                />
+              </div>
+            </div>
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
+  );
+}
