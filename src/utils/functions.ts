82f15d15e9ac4dc8815992e78a928d2cc2ab5b3c
Mubin Ansari <mubinansari910@gmail.com>
2023-01-28 03:08:28 +0530

    add not allowed slugs list and move slug characters to constants

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,10 +1,8 @@
+import { SLUG_CHARACTERS } from "$constants";
 import { customAlphabet } from "nanoid";
 
 export function isValidEmail(email: string) {
   return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
 }
 
-export const nanoidForSlug = customAlphabet(
-  "0123456789abcdefghijklmnopqrstuvwxyz",
-  5,
-);
+export const nanoidForSlug = customAlphabet(SLUG_CHARACTERS, 5);
