550d0cf1eaefeb0be89ae3619fbdce071926d83d
Mubin Ansari <mubinansari910@gmail.com>
2023-01-30 01:48:30 +0530

    remove svg and use their react icons counterpart

--- src/components/AuthModal.tsx
+++ src/components/AuthModal.tsx
@@ -4,9 +4,9 @@ import { Dialog, Transition } from "@headlessui/react";
 import { useAtom } from "jotai";
 import { signIn } from "next-auth/react";
 import { useTheme } from "next-themes";
-import Image from "next/image";
 import { Fragment, useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
+import { FcGoogle } from "react-icons/fc";
 import { HiMailOpen } from "react-icons/hi";
 import Button from "./Button";
 import Input from "./Input";
@@ -172,7 +172,7 @@ const AuthModal = () => {
               onClick={signInWithGoogle}
               className="mx-auto flex h-[46px] w-full items-center justify-center space-x-2 rounded-md border p-2 text-gray-500 transition-colors hover:border-gray-400 hover:bg-gray-50 hover:text-gray-600 focus:outline-none focus:ring-4 focus:ring-gray-400 focus:ring-opacity-25 disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:border-gray-200 disabled:hover:bg-transparent disabled:hover:text-gray-500 dark:border-gray-500 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-gray-200 dark:focus:ring-gray-600"
             >
-              <Image src="/google.svg" alt="Google" width={32} height={32} />
+              <FcGoogle className="h-8 w-8" />
               <span>Sign in with Google</span>
             </button>
           </div>
