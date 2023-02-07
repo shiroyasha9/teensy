632ca8815652c95ed9e4197f01af04cd78a890bb
Mubin Ansari <mubinansari910@gmail.com>
2023-02-07 22:21:36 +0530

    remove unnecessary middleware calls

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,5 +24,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/:slug*", "/wa/:number*"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|teensies).*)"],
 };
