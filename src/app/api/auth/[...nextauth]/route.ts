31aae2b305531ad8123658adcc77b3c74ed3bd0e
Mubin Ansari <mubinansari910@gmail.com>
2023-08-25 20:46:30 +0530

    Move auth from lib to server folder

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import { authOptions } from "@/lib/auth";
+import { authOptions } from "@/server/auth";
 import NextAuth from "next-auth/next";
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
