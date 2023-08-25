aceec1504e1550b86c021330ada31addefab47ae
Mubin Ansari <mubinansari910@gmail.com>
2023-08-25 12:20:34 +0530

    Import SwaggerUI as dynamic import

--- src/app/swagger/page.tsx
+++ src/app/swagger/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import type { NextPage } from "next";
-import SwaggerUI from "swagger-ui-react";
+import dynamic from "next/dynamic";
 import "swagger-ui-react/swagger-ui.css";
+
+const SwaggerUI = dynamic(() => import("swagger-ui-react"), { ssr: false });
 
 const SwaggerPage: NextPage = () => {
   return (
