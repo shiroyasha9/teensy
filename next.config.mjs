ed0fbfff9a0d1bcb9a9d7456140a1df5540b7620
Mubin Ansari <mubinansari910@gmail.com>
2023-08-30 16:00:25 +0530

    Fix route not being intercepted

--- next.config.mjs
+++ next.config.mjs
@@ -8,12 +8,6 @@
 const config = {
   reactStrictMode: true,
   swcMinify: true,
-  /* If trying out the experimental appDir, comment the i18n config out
-   * @see https://github.com/vercel/next.js/issues/41980 */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
 };
 
 const withPWA = (await import("next-pwa")).default({
