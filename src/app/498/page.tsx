219e491b17cb12fa08192b1a02d3e65d1e53af80
Mubin Ansari <mubinansari910@gmail.com>
2023-08-30 20:49:53 +0530

    Move 498.tsx to app router

--- src/app/498/page.tsx
+++ src/app/498/page.tsx
@@ -1,16 +1,14 @@
 import Button from "@/components/Button";
 import Link from "next/link";
-let origin = "";
-if (typeof window !== "undefined") origin = window.location.origin;
 
-export default function show498() {
+export default function ExpiredTeensy() {
   return (
     <main className="flex h-screen flex-col items-center justify-center">
       <h1 className="mb-3 text-9xl font-bold">498</h1>
       <p className="text-center">
         teensy link has expired, contact the owner, create this teensy or
       </p>
-      <Link href={origin}>
+      <Link href="/">
         <Button title="Go back to home page 🏠" />
       </Link>
     </main>
