26d13aad16c2c20bcdbf87b5c405e8efbcffbf9e
Mubin Ansari <mubinansari910@gmail.com>
2023-08-30 17:37:50 +0530

    Move 404 page to app router

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,12 @@
 import Button from "@/components/Button";
 import Link from "next/link";
-let origin = "";
-if (typeof window !== "undefined") origin = window.location.origin;
 
-export default function show404() {
+export default function NotFound() {
   return (
     <main className="flex h-screen flex-col items-center justify-center">
-      <h1 className="mb-3 text-9xl font-bold">404 </h1>
+      <h1 className="mb-3 text-9xl font-bold">404</h1>
       <p>Invalid alias, check for typos, or</p>
-      <Link href={origin}>
+      <Link href="/">
         <Button title="Go back to home page 🏠" />
       </Link>
     </main>
