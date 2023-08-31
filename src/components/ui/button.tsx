1a85da1b2b2df1351477d16f3feb4f53cd26d7d2
Mubin Ansari <mubinansari910@gmail.com>
2023-09-01 03:09:08 +0530

    Add tertiary style to button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,6 +20,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        tertiary: "bg-purple-600 hover:bg-purple-900 text-white",
       },
       size: {
         default: "h-9 px-4 py-2",
@@ -37,7 +38,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-  VariantProps<typeof buttonVariants> {
+    VariantProps<typeof buttonVariants> {
   isLoading?: boolean;
 }
 
