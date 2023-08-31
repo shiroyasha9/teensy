f48bfcd895627e65c2c54cd704e030be48fe7f9b
Mubin Ansari <mubinansari910@gmail.com>
2023-09-01 02:28:50 +0530

    Remove h-screen from 404 and 498 pages

--- src/app/498/page.tsx
+++ src/app/498/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function ExpiredTeensy() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
+    <main className="flex flex-col items-center justify-center">
       <h1 className="mb-3 text-9xl font-bold">498</h1>
       <p className="text-center">
         teensy link has expired, contact the owner, create this teensy or
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
+    <main className="flex flex-col items-center justify-center">
       <h1 className="mb-3 text-9xl font-bold">404</h1>
       <p>Invalid alias, check for typos, or</p>
       <Link href="/">
