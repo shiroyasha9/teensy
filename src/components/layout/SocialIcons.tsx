5c2b23f73a38d7fe10f4d9b31e9d69406c53bcd1
Mubin Ansari <mubinansari910@gmail.com>
2023-09-15 01:28:26 +0530

    Use anchor tag instead of links in socialIcons for cors error

--- src/components/layout/SocialIcons.tsx
+++ src/components/layout/SocialIcons.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { AiOutlineGithub } from "react-icons/ai";
 import { IoLogoGooglePlaystore } from "react-icons/io5";
 import { SiVisualstudio } from "react-icons/si";
@@ -6,21 +5,21 @@ import { SiVisualstudio } from "react-icons/si";
 export default function SocialIcons() {
   return (
     <div className="flex items-center gap-2">
-      <Link href="/gh" target="_blank" rel="noreferrer">
+      <a href="/gh" target="_blank" rel="noreferrer">
         <AiOutlineGithub className="h-6 w-6" aria-label="teensy github page" />
-      </Link>
-      <Link href="/vscode" target="_blank" rel="noreferrer">
+      </a>
+      <a href="/vscode" target="_blank" rel="noreferrer">
         <SiVisualstudio
           className="h-5 w-5"
           aria-label="teensy vs code extension"
         />
-      </Link>
-      <Link href="/ps" target="_blank" rel="noreferrer">
+      </a>
+      <a href="/ps" target="_blank" rel="noreferrer">
         <IoLogoGooglePlaystore
           className="h-6 w-6"
           aria-label="teensy play store app"
         />
-      </Link>
+      </a>
     </div>
   );
 }
