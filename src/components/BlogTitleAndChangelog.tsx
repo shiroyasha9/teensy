cd17fdf71d8853f22678aee1ddb065d97e1997b6
Anuj <anujjoshi63@gmail.com>
2023-11-06 04:34:23 -0700

    add props to BlogTitleAndChangelog

---
+++ src/components/BlogTitleAndChangelog.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+type BlogTitleAndChangelogProps = {
+  tabTitle: string;
+  blogTitle: string;
+  blogSubtitle: string;
+  changeLogURL: string;
+};
+
+export const BlogTitleAndChangelog = ({
+  tabTitle,
+  blogTitle,
+  blogSubtitle,
+  changeLogURL,
+}: BlogTitleAndChangelogProps) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <title>{tabTitle}</title>
+      <div className=" text-5xl font-medium text-white opacity-100">
+        {blogTitle} <br />
+        <span className="font-medium">{blogSubtitle}</span>
+      </div>
+      <div className="font-extralight text-white">
+        What's new?&nbsp;
+        <Link target="_blank" className="font-extralight" href={changeLogURL}>
+          Changelog on GitHub.
+        </Link>
+      </div>
+    </div>
+  );
+};
