344744ac584b4428bff9646adbc6ab4cea4b4f3a
Mubin Ansari <mubinansari910@gmail.com>
2024-04-30 16:10:51 +0530

    Fix drizzle studio

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "drizzle-kit";
 import { env } from "./src/env.mjs";
 
 export default {
-  schema: "./src/server/schema.ts",
+  schema: "./src/server/schema/index.ts",
   driver: "pg",
   strict: true,
   verbose: true,
