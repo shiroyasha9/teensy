a0d3858de1f5dd5416cb0705f2c47c1df9fa6d4b
Mubin Ansari <mubinansari910@gmail.com>
2024-05-01 01:20:27 +0530

    Fix errors in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,27 +3,21 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
-			url: "teensy.tech",
+			url: "https://teensy.tech",
 			lastModified: new Date(),
 			changeFrequency: "weekly",
 			priority: 1,
 		},
 		{
-			url: "teensy.tech/blog",
+			url: "https://teensy.tech/blog",
 			lastModified: new Date(),
 			changeFrequency: "weekly",
 			priority: 0.8,
 		},
 		{
-			url: "teensy.tech/wa",
+			url: "https://teensy.tech/wa",
 			lastModified: new Date(),
 			changeFrequency: "yearly",
-			priority: 0.5,
-		},
-		{
-			url: "https://github.com/shiroyasha9/teensy",
-			lastModified: new Date(),
-			changeFrequency: "monthly",
 			priority: 0.5,
 		},
 	];
