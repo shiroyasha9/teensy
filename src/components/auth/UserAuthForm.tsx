c2fe7da1c5bf8595bde874f60149b832b85ee225
Mubin Ansari <mubinansari910@gmail.com>
2024-10-22 11:53:15 +0530

    Fix issue where login modal would crash with "DYNAMIC_SERVER_USAGE"

--- src/components/auth/UserAuthForm.tsx
+++ src/components/auth/UserAuthForm.tsx
@@ -1,30 +1,26 @@
 "use client";
 
 import { signInServerFn } from "@/server/functions";
-import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
 import { FcGoogle } from "react-icons/fc";
 import { toast } from "sonner";
 
 const UserAuthForm = () => {
-	const [isLoading, setIsLoading] = useState(false);
-
-	const signInWithGoogle = async () => {
-		toast.loading("Redirecting...");
-		setIsLoading(true);
-		try {
-			await signInServerFn();
-		} catch (_err) {
+	const { mutate: signInHandler, isPending } = useMutation({
+		mutationFn: async () => await signInServerFn(),
+		onMutate: () => {
+			toast.loading("Redirecting...");
+		},
+		onError: () => {
 			toast.error("There was an error signing in.");
-		} finally {
-			setIsLoading(false);
-		}
-	};
+		},
+	});
 
 	return (
 		<button
 			type="button"
-			disabled={isLoading}
-			onClick={signInWithGoogle}
+			disabled={isPending}
+			onClick={() => signInHandler()}
 			className="mx-auto flex h-[46px] w-full items-center justify-center space-x-2 rounded-md border p-2 text-zinc-500 transition-colors hover:border-zinc-400 hover:bg-zinc-50 hover:text-zinc-600 focus:outline-none focus:ring-4 focus:ring-zinc-400 focus:ring-opacity-25 disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:border-zinc-200 disabled:hover:bg-transparent disabled:hover:text-zinc-500 dark:border-zinc-500 dark:text-zinc-300 dark:hover:bg-zinc-600 dark:hover:text-zinc-200 dark:focus:ring-zinc-600"
 		>
 			<FcGoogle className="h-8 w-8" />
