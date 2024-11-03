b46b9597916764444c8dbdd5065f756fba6a156f
Mubin Ansari <mubinansari910@gmail.com>
2024-11-04 03:37:28 +0530

    Move non logged in case in AuthButton at the start

--- src/features/header/components/AuthButton.tsx
+++ src/features/header/components/AuthButton.tsx
@@ -24,66 +24,68 @@ type AuthButtonProps = {
 };
 
 const AuthButton = ({ name, email, image }: AuthButtonProps) => {
-	if (name && email) {
+	if (!(name && email)) {
 		return (
-			<>
-				<div className="flex flex-col gap-y-2 px-2 pb-2 md:hidden">
-					<div className="flex items-center gap-x-2">
-						<Avatar className="size-6">
-							<AvatarImage src={image ?? undefined} />
-							<AvatarFallback>
-								<AvatarIcon className="size-full" />
-							</AvatarFallback>
-						</Avatar>
-						<div className="flex flex-col">
-							<Label>{name}</Label>
-							<p className="text-muted-foreground text-xs">{email}</p>
-						</div>
-					</div>
-					<MenubarSeparator />
-					<Button
-						variant="ghost"
-						className="h-auto justify-start p-0 font-normal text-sm"
-						onClick={signOutServerFn}
-					>
-						Logout
-					</Button>
-				</div>
-				<DropdownMenu>
-					<DropdownMenuTrigger className="hidden md:flex md:px-4">
-						<Avatar className="size-6">
-							<AvatarImage src={image ?? undefined} />
-							<AvatarFallback>
-								<AvatarIcon className="size-full" />
-							</AvatarFallback>
-						</Avatar>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent align="end">
-						<DropdownMenuLabel>{name}</DropdownMenuLabel>
-						<DropdownMenuItem>{email}</DropdownMenuItem>
-						<DropdownMenuSeparator />
-						<DropdownMenuItem
-							className="cursor-pointer"
-							onClick={signOutServerFn}
-						>
-							Logout
-						</DropdownMenuItem>
-					</DropdownMenuContent>
-				</DropdownMenu>
-			</>
+			<Link
+				href="/login"
+				className={cn(
+					buttonVariants({
+						size: "sm",
+						className: "font-semibold text-base",
+					}),
+				)}
+			>
+				Login
+			</Link>
 		);
 	}
 
 	return (
-		<Link
-			href="/login"
-			className={cn(
-				buttonVariants(),
-				"m-0 h-auto w-full px-4 py-2 font-semibold text-base md:ml-8 md:w-auto md:px-3 md:py-1",
-			)}
-		>
-			Login
-		</Link>
+		<>
+			<div className="flex flex-col gap-y-2 px-2 pb-2 md:hidden">
+				<div className="flex items-center gap-x-2">
+					<Avatar className="size-6">
+						<AvatarImage src={image ?? undefined} />
+						<AvatarFallback>
+							<AvatarIcon className="size-full" />
+						</AvatarFallback>
+					</Avatar>
+					<div className="flex flex-col">
+						<Label>{name}</Label>
+						<p className="text-muted-foreground text-xs">{email}</p>
+					</div>
+				</div>
+				<MenubarSeparator />
+				<Button
+					variant="ghost"
+					className="h-auto justify-start p-0 font-normal text-sm"
+					onClick={signOutServerFn}
+				>
+					Logout
+				</Button>
+			</div>
+			<DropdownMenu>
+				<DropdownMenuTrigger className="hidden md:flex md:px-4">
+					<Avatar className="size-6">
+						<AvatarImage src={image ?? undefined} />
+						<AvatarFallback>
+							<AvatarIcon className="size-full" />
+						</AvatarFallback>
+					</Avatar>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent align="end">
+					<DropdownMenuLabel>{name}</DropdownMenuLabel>
+					<DropdownMenuItem>{email}</DropdownMenuItem>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem
+						className="cursor-pointer"
+						onClick={signOutServerFn}
+					>
+						Logout
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</>
 	);
 };
 
